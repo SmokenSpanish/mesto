4eaeffc63c5311099eff616ca0d4ddfc17b29dc6
Spanish <maximbasov1992@mail.ru>
2020-12-26 02:00:44 +0300

    firstCheck

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,59 @@
-let profile = document.querySelector('.profile');
-let popup = document.querySelector('.popup');
-let popupPlace = document.querySelector('.popup-place');
-let popupImage = document.querySelector('.popup-image');
-let popupCloseButton = popup.querySelector('.popup__close-button');
-let popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
-let popupImageCloseButton = popupImage.querySelector('.popup__image-button');
-let editButton = profile.querySelector('.profile__edit-button');
-let addButton = profile.querySelector('.profile__add-button');
-let nameInputProfile = profile.querySelector('.profile__name');
-let nameInputPopup = popup.querySelector('.popup__input_type_name');
-let placeNameInputPopup = popupPlace.querySelector('.popup__input_type_placeName');
-let aboutInputProfile = profile.querySelector('.profile__about');
-let aboutInputPopup = popup.querySelector('.popup__input_type_description');
-let LinkInputPopup = popupPlace.querySelector('.popup__input_type_imgLink');
-let popupSaveButton = document.querySelector('.popup__button');
-let popupCreateButton = popupPlace.querySelector('.popup__create');
+const profile = document.querySelector('.profile');
+const popup = document.querySelector('.popup');
+const popupPlace = document.querySelector('.popup-place');
+const popupImage = document.querySelector('.popup-image');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
+const popupImageCloseButton = popupImage.querySelector('.popup__image-button');
+const editButton = profile.querySelector('.profile__edit-button');
+const addButton = profile.querySelector('.profile__add-button');
+const nameInputProfile = profile.querySelector('.profile__name');
+const nameInputPopup = popup.querySelector('.popup__input_type_name');
+const placeNameInputPopup = popupPlace.querySelector('.popup__input_type_placeName');
+const aboutInputProfile = profile.querySelector('.profile__about');
+const aboutInputPopup = popup.querySelector('.popup__input_type_description');
+const LinkInputPopup = popupPlace.querySelector('.popup__input_type_imgLink');
+const popupSaveButton = document.querySelector('.popup__button');
+const popupCreateButton = popupPlace.querySelector('.popup__create');
+const popupPic = popupImage.querySelector('.popup__image');
 
-
-function popupActionOpen(){
-    nameInputPopup.value = nameInputProfile.textContent;
-    aboutInputPopup.value = aboutInputProfile.textContent;
+function popupActionOpen(popup){
     popup.classList.add('popup_opened');
 }
 
-function popupPlaceActionOpen(){
-    popupPlace.classList.add('popup_opened');
-}
-
-
-
-function popupPlaceActionClose(){
-    popupPlace.classList.remove('popup_opened');
-}
-
-function popupImageActionClose(){
-    popupImage.classList.remove('popup_opened');
-}
-
-function popupActionClose(){
+function popupActionClose(popup){
     popup.classList.remove('popup_opened');
 }
-
 
 function saveInputValue(evt) {
     evt.preventDefault();
     nameInputProfile.textContent = nameInputPopup.value;
     aboutInputProfile.textContent = aboutInputPopup.value;
-    popupActionClose();
+    popupActionClose(popup);
 }
 
+editButton.addEventListener('click', function(){
+    nameInputPopup.value = nameInputProfile.textContent;
+    aboutInputPopup.value = aboutInputProfile.textContent;
+    popupActionOpen(popup);
+});
 
-editButton.addEventListener('click',popupActionOpen);
-addButton.addEventListener('click', popupPlaceActionOpen);
-popupCloseButton.addEventListener('click', popupActionClose);
-popupPlaceCloseButton.addEventListener('click', popupPlaceActionClose);
-popupImageCloseButton.addEventListener('click', popupImageActionClose);
+popupCloseButton.addEventListener('click', function(){
+    popupActionClose(popup);
+});
+
+addButton.addEventListener('click', function(){
+    popupActionOpen(popupPlace);
+});
+
+popupPlaceCloseButton.addEventListener('click', function(){
+    popupActionClose(popupPlace);
+});
+
+popupImageCloseButton.addEventListener('click', function(){
+    popupActionClose(popupImage);
+});
+
 popup.addEventListener('submit', saveInputValue);
 
 
@@ -109,16 +108,18 @@ function composeItem(item){
     return newItem;
 }
 
-popupCreateButton.addEventListener('click', addNewItem);
 
-function addNewItem(evt){
+
+function addNewItem(evt) {
     evt.preventDefault();
     const inputText = placeNameInputPopup.value;
     const inputLink = LinkInputPopup.value;
     const newItem = composeItem({name: inputText, link: inputLink});
     userContainerElements.prepend(newItem);
-    popupPlaceActionClose();
+    popupActionClose(popupPlace);
 } 
+
+popupPlace.addEventListener('submit', addNewItem);
 
 function removeItem(e){
     const targetElement = e.target;
@@ -129,13 +130,12 @@ function removeItem(e){
 function actionLike(e){
     const targetLike = e.target;
     const targetItem = targetLike.closest('.element__rectangle');
-    targetItem.classList.add('element__rectangle_active');
+    targetItem.classList.toggle('element__rectangle_active');
 }
 
 
 function PopupImageActionOpen(e){
     const targetElement = e.target;
-    const popupPic = popupImage.querySelector('.popup__image');
     const popupImageTitle = document.querySelector('.popup__image-title');
     // popupImageTitle.textContent = targetElement.closest('.element__title').textContent; почему то не работает:{
     popupImageTitle.textContent = document.querySelector('.element__title').textContent;
